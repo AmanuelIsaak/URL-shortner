5583d030e29f6d8447548009ac4bba6559609dbb
AmanuelIsaak <amanuelisaak15@gmail.com>
2024-10-08 12:33:46 +0200

    initial commit

--- main.go
+++ main.go
@@ -4,28 +4,61 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
 )
 
 type PageData struct {
-	Name string
+	Name  string
+	Short string
 }
+
+type shortURL struct {
+	OriginalURL string `json:"original_url"`
+	ShortCode   string `json:"short_code"`
+}
+
+var urlMap = make(map[string]shortURL)
 
 func main() {
 	tmpl := template.Must(template.New("").ParseGlob("./templates/*"))
 
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "index.html", PageData{
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.ExecuteTemplate(w, "index.html", PageData{
 			Name: "YouTube!",
 		})
+		if err != nil {
+			return
+		}
 	})
 
-	router.HandleFunc("GET /shorten", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "shorten.html", PageData{
-			Name: "YouTube!",
-		})
+	router.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			originalURL := r.FormValue("url")
+			shortCode := generateShortCode()
+			urlMap[shortCode] = shortURL{OriginalURL: originalURL, ShortCode: shortCode}
+
+			err := tmpl.ExecuteTemplate(w, "shorten.html", PageData{
+				Name:  "YouTube!",
+				Short: "http://localhost:8080/r/" + shortCode,
+			})
+			if err != nil {
+				return
+			}
+		} else {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		}
+	})
+
+	router.HandleFunc("/r/", func(w http.ResponseWriter, r *http.Request) {
+		shortCode := r.URL.Path[len("/r/"):]
+		if url, ok := urlMap[shortCode]; ok {
+			http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+		} else {
+			http.NotFound(w, r)
+		}
 	})
 
 	srv := http.Server{
@@ -39,4 +72,13 @@ func main() {
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		fmt.Println("An error occured:", err)
 	}
+}
+
+func generateShortCode() string {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	shortCode := make([]byte, 6)
+	for i := range shortCode {
+		shortCode[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(shortCode)
 }
